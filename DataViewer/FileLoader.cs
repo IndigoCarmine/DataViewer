53fb279a8e789e7a9428dc4a58e74cd8143185e2
IndigoCarmine <tamanisikaminai@gmail.com>
2024-01-31 21:45:09 +0900

    プロジェクト ファイルを追加します。

---
+++ DataViewer/FileLoader.cs
@@ -0,0 +1,204 @@
+﻿using System.IO;
+using System.Text;
+
+namespace DataViewer
+{
+    static class LoaderFactory
+    {
+        public static FileLoader Create(string filepath, Type type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    abstract class FileLoader{
+        protected string filepath;
+        protected double[]? _x;
+        protected double[]? _y;
+        public double[] x 
+        { 
+            get 
+            {
+                if (_x != null) return _x;
+                
+                var data = LoadValue();
+                if (data != null)return data.Item1;
+                return new double[0];
+            }
+            protected set { _x = value; }
+        }
+        public double[] y
+        {
+            get
+            {
+                if (_y != null) return _y;
+
+                var data = LoadValue();
+                if (data != null) return data.Item2;
+                return new double[0];
+            }
+            protected set { _y = value; }
+        }
+
+        public FileLoader(string filepath)
+        {
+            this.filepath = filepath;
+        }
+
+        abstract public Tuple<double[], double[]>? LoadValue();
+
+    }
+
+    class NMRFileLoader : FileLoader
+    {
+        public NMRFileLoader(string filepath) : base(filepath)
+        {
+        }
+
+        public override Tuple<double[], double[]>? LoadValue()
+        {
+
+            List<int> temp_x = new List<int>();
+            List<int> temp_y = new List<int>();
+            using(var fs = new FileStream(filepath,FileMode.Open))
+            {
+
+                byte[] buffer = new byte[8];
+                //read 4byte and cast as float32
+                while(true)
+                {
+
+                    int e = fs.Read(buffer, 0, 8);
+                    if(e == 0) break;
+                    var rev = buffer.Reverse().ToArray();
+                    temp_x.Add(BitConverter.ToInt16(rev, 0));
+                    temp_y.Add(BitConverter.ToInt16(rev, 4));
+
+                }
+            }
+
+
+
+            x = temp_x.Select(e=>(double)e).ToArray();
+            y = temp_y.Select(e=>(double)e).ToArray();
+
+
+            return Tuple.Create(x, y);
+        }
+
+
+        private int toInt16(byte[] data)
+        {
+            return BitConverter.ToInt32(data.Reverse().ToArray(), 0);
+        }
+
+    }
+
+
+
+    class FtIrFileLoader : FileLoader
+    {
+        public FtIrFileLoader(string filepath) : base(filepath)
+        {
+        }
+
+        public override Tuple<double[], double[]>? LoadValue()
+        {
+            bool is_data = false;
+
+
+
+            List<double> temp_x = new List<double>();
+            List<double> temp_y = new List<double>();
+
+
+            using (var fs = new StreamReader(filepath, Encoding.UTF8))
+            {
+                while (true)
+                {
+                    string? line = fs.ReadLine();
+                    if (line == null) break;
+                    if (line == "XYDATA") { is_data = true; continue; }
+
+                    if (!is_data) continue;
+                    if (line == "") break;
+                    var line_data = line.Split('\t');
+                    temp_x.Add(double.Parse(line_data[0]));
+                    temp_y.Add(double.Parse(line_data[1]));
+
+                }
+            }
+
+            x = temp_x.ToArray();
+            y = temp_y.ToArray();
+
+            return Tuple.Create(x, y);
+        }
+    }
+
+    class CSVFileLoader : FileLoader
+    {
+
+        bool has_header;
+
+        public CSVFileLoader(string filepath,bool has_header) : base(filepath)
+        {
+            this.has_header = has_header;
+        }
+
+        public override Tuple<double[], double[]>? LoadValue()
+        {
+            List<double> x = new List<double>();
+            List<double> y = new List<double>();
+
+
+            using (var fs = new StreamReader(filepath, Encoding.UTF8))
+            {
+                if(has_header)fs.ReadLine();
+                while (true)
+                {
+                    string? line = fs.ReadLine();
+                    if (line == null) break;
+                    var line_data = line.Split(',');
+                    x.Add(double.Parse(line_data[0]));
+                    y.Add(double.Parse(line_data[1]));
+
+                }
+            }
+
+            return Tuple.Create(x.ToArray(), y.ToArray());
+        }
+    }
+
+    class TSVFileLoader : FileLoader
+    {
+        bool has_header;
+        public TSVFileLoader(string filepath,bool has_header) : base(filepath)
+        {
+            this.has_header = has_header;
+        }
+
+        public override Tuple<double[], double[]>? LoadValue()
+        {
+            List<double> x = new List<double>();
+            List<double> y = new List<double>();
+
+
+            using (var fs = new StreamReader(filepath, Encoding.UTF8))
+            {
+                if(has_header) fs.ReadLine();
+                while (true)
+                {
+                    string? line = fs.ReadLine();
+                    if (line == null) break;
+                    var line_data = line.Split('\t');
+                    x.Add(double.Parse(line_data[0]));
+                    y.Add(double.Parse(line_data[1]));
+
+                }
+            }
+
+            return Tuple.Create(x.ToArray(), y.ToArray());
+        }
+    }
+}
---
+++ DataViewer/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using ScottPlot;
+using ScottPlot.Plottables;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FileLoader? loader;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ScottPlot.Control.InputBindings customInputBindings = new()
+            {
+                DragPanButton = ScottPlot.Control.MouseButton.Middle,
+                DragZoomRectangleButton = ScottPlot.Control.MouseButton.Right,
+                DragZoomButton = ScottPlot.Control.MouseButton.Right,
+                ZoomInWheelDirection = ScottPlot.Control.MouseWheelDirection.Up,
+                ZoomOutWheelDirection = ScottPlot.Control.MouseWheelDirection.Down,
+                ClickAutoAxisButton = ScottPlot.Control.MouseButton.Right,
+                ClickContextMenuButton = ScottPlot.Control.MouseButton.Left,
+            };
+
+            ScottPlot.Control.Interaction interaction = new(WpfPlot1)
+            {
+                Inputs = customInputBindings,
+            };
+            WpfPlot1.Plot.ScaleFactor = 3;
+            WpfPlot1.Plot.FigureBackground = ScottPlot.Colors.Transparent;
+            WpfPlot1.Plot.DataBackground = ScottPlot.Colors.Transparent;
+            WpfPlot1.Plot.Style.ColorAxes(ScottPlot.Color.Gray(255));
+
+
+            WpfPlot1.Interaction = interaction;
+            WpfPlot1.Refresh();
+        }
+
+        private void Grid_Drag(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+                return;
+
+            //MessageBox.Show("dropped");
+
+            var dropFiles = e.Data.GetData(DataFormats.FileDrop) as string[];
+
+            if (dropFiles == null)
+                return;
+
+            foreach(var file in dropFiles)
+            {
+                if (File.Exists(file))
+                {
+                   loader = new TSVFileLoader(file, false);
+
+                    var data = loader.LoadValue();
+                    if (data == null) return;
+                    //MessageBox.Show(string.Join(',',Array.ConvertAll(data.Item1,e=>e.ToString())));
+                    RangeSlider.Maximum = data.Item1.Count();
+                    RangeSlider.Minimum = 0;
+                    
+                    var scatter = WpfPlot1.Plot.Add.Scatter(data.Item1, data.Item2,color:ScottPlot.Colors.White);
+                    scatter.MarkerStyle = MarkerStyle.None;
+ 
+                    //WpfPlot1.Plot.Axes.SetLimitsX(1, -1);
+                    WpfPlot1.Plot.Axes.AutoScale();
+                    WpfPlot1.Refresh();
+                }
+                else
+                {
+                    MessageBox.Show("deuebfei");
+                }
+            }
+        }
+
+        private void Range_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (loader == null) return;
+            WpfPlot1.Plot.Clear();
+            var seg_x = new ArraySegment<double>(loader.x);
+            var seg_y = new ArraySegment<double>(loader.y);
+            var scatter = WpfPlot1.Plot.Add.Scatter(seg_x.Slice((int)e.NewValue).ToArray(), seg_y.Slice((int)e.NewValue).ToArray(),color:ScottPlot.Colors.White);
+            scatter.MarkerStyle = MarkerStyle.None;
+            //WpfPlot1.Plot.Axes.SetLimitsX(1, -1);
+            WpfPlot1.Refresh();
+
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            WpfPlot1.Plot.Style.AxisFrame(1, 0, 1, 0);
+            WpfPlot1.Refresh();
+        }
+
+        private void WpfPlot1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point p = e.GetPosition(this);
+            Pixel mousePixel = new(p.X,p.Y);
+            Coordinates mouseLocation = WpfPlot1.Plot.GetCoordinates(mousePixel);
+            DataPoint? nearest = (WpfPlot1.Plot.PlottableList.FirstOrDefault() as Scatter)?.Data.GetNearest(mouseLocation, WpfPlot1.Plot.LastRender);
+
+
+            if (nearest != null && nearest.HasValue)
+            {
+                NearestPoint.Text = nearest.Value.X.ToString() + ":" + nearest.Value.Y.ToString();
+            }
+        }
+    }
+}
